3e34edcc0c4ae447176b1b8db548f7fcf1c48287
Kishoreadhith V <117294726+kishoreadhith-v@users.noreply.github.com>
2025-03-26 21:46:29 +0530

    Update sumbitCode.ts

--- src/services/sumbitCode.ts
+++ src/services/sumbitCode.ts
@@ -1,18 +1,31 @@
 import judgeapi from '../API/judge0';
+import axios from 'axios';
+
 interface submitCodeArgs {
   code: string;
   language_id: number;
   input: string;
   expected_output: string;
+  userId: string;
+  difficulty: string;
 }
+
 async function submitCode(params: submitCodeArgs) {
   try {
+    console.log('Code being submitted:', params.code);
     const response = await judgeapi.post('/submissions', {
       source_code: params.code,
       language_id: params.language_id,
       stdin: params.input.replace('\\n', '\n'),
       expected_output: params.expected_output,
     });
+
+    if (response.data.status.id === 3) { // Assuming 3 is the ID for "Accepted" status
+      await axios.patch(`/api/users/${params.userId}/update-score`, {
+        difficulty: params.difficulty
+      });
+    }
+
     return response;
   } catch (error) {
     if (error instanceof Error) {
@@ -20,4 +33,5 @@ async function submitCode(params: submitCodeArgs) {
     }
   }
 }
+
 export default submitCode;
